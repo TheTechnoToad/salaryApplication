e202279b8e4a77f1aa75fbeac65c1a006902b9bc
sxp362 <sqielyr.person-johnson@equifax.com>
2018-01-08 15:27:20 -0500

    App initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
